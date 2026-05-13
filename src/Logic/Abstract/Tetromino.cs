a0a5325dd2810fc25daede2079d0072e7e09cb15
Liza Vyborna <vubornaliza20@gmail.com>
2026-05-13 16:30:46 +0300

    Виправлення

--- src/Logic/Abstract/Tetromino.cs
+++ src/Logic/Abstract/Tetromino.cs
@@ -3,12 +3,14 @@ public abstract class Tetromino
 {
     public int X { get; set; }
     public int Y { get; set; }
-    protected int[,] shape; // Додаємо поле для зберігання поточної форми
+    protected int[,]? shape;
 
-    public virtual int[,] GetShape() => shape;
+    public virtual int[,] GetShape() => shape ?? new int[0, 0];
 
     public virtual void Rotate()
     {
+        if (shape == null) return;
+        
         int rows = shape.GetLength(0);
         int cols = shape.GetLength(1);
         int[,] newShape = new int[cols, rows];
@@ -17,8 +19,6 @@ public abstract class Tetromino
         {
             for (int x = 0; x < cols; x++)
             {
-                // Магія повороту: новий стовпець = старий рядок
-                // новий рядок = ширина - 1 - старий стовпець
                 newShape[x, rows - 1 - y] = shape[y, x];
             }
         }
